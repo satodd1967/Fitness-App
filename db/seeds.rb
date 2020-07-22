3122dfe88602b89932ff2ebe539d30e5a8f2264e
satodd1967 <seth.todd@outlook.com>
2020-07-22 12:34:03 -0500

    wrote seeds data stub

---
+++ db/seeds.rb
@@ -0,0 +1,3 @@
+kris = User.create(username: "KrisDecker", email: "kris@outlook.com", password: "kris1", start_weight: "120", start_bodyfat: "15.5")
+Goal.create(weight_goal: "112", body_fat_goal: "10.5", start_calorie_goal: "2800", user: kris)
+log1 = Log.create(worked_out: "1", tracked_food: "1", weight: "124", body_fat: "14.8", active_calories: "800", calories: "3000", user: kris)
