3e333572f3cf4aa3a499219970aea0e89e426aa9
satodd1967 <seth.todd@outlook.com>
2020-07-27 22:28:00 -0500

    went through the app and added navigation points for the personal diary and the public diary on applicable pages.  setup a validation when a user logs in to check if they have goals.  If they do not have goals then they are redirected to the goals page to enter thier goals.  Added a validation to the personal diary page that checks to see if the user has any diary entries.  If they do not they are rerouted to the new log entry page with a message that tells them they have no diary entries and to enter the first one below.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,8 +3,13 @@ class UsersController < ApplicationController
     post '/login' do
         user = User.find_by(username: params[:username])
 		if user && user.authenticate(params[:password])
-		  session[:user_id] = user.id
-		  redirect '/logs'
+          session[:user_id] = user.id
+          if user.goal == nil
+            flash[:notice] = "You have not entered your goals yet.  Please enter your goals to continue"
+            redirect '/goals/new'
+          else
+            redirect '/logs'
+          end
         else
             flash[:notice] = "Failed to login.  Please try again."
 		    redirect '/'
@@ -22,7 +27,7 @@ class UsersController < ApplicationController
 
     get '/signup' do
         if logged_in?
-            flash[:notice] = "You are currently already logged in.  Please logout to create a new user account"
+            flash[:notice] = "You are currently logged in.  Please logout to create a new user account"
             redirect '/'
         end
         erb :"/users/create_user" 
@@ -55,7 +60,14 @@ class UsersController < ApplicationController
     get '/users/:slug' do
         @user = User.find_by_slug(params[:slug])
         @logs = @user.logs
-        erb :'users/show'
+        binding.pry
+        if @logs == []
+            flash[:notice] = "You don't currently have any Diary Entries."
+            flash[:navigation] = "Create your first entry below."
+            redirect '/logs/new'
+        else
+            erb :'users/show'
+        end
     end
 
 end
