243e90e4ac0726faced7aca278e5c53a48120726
satodd1967 <seth.todd@outlook.com>
2020-08-01 21:48:54 -0500

    added markup for the ClearnController and the self.clearn method.

--- app/controllers/clean_controller.rb
+++ app/controllers/clean_controller.rb
@@ -1,5 +1,7 @@
 class CleanController < ApplicationController
 
+    # This model and method are used to clear the database so that it can be re-seeded.
+
     def self.clean
         User.destroy_all
         Log.destroy_all
