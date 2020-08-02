d25258476e1a50c7b7581c62c2f55376b1872b1c
satodd1967 <seth.todd@outlook.com>
2020-08-02 12:46:24 -0500

    changed the create method in the goals new route to utilize the params form of create vs. each individual key and value pair.

--- app/controllers/goals_controller.rb
+++ app/controllers/goals_controller.rb
@@ -11,14 +11,10 @@ class GoalsController < ApplicationController
         user_check
         check_completion(params)
         @user = current_user
-        @goals = Goal.create(
-            start_weight: params[:start_weight],
-            start_bodyfat: "#{params[:start_bodyfat].to_f/100}",
-            weight_goal: params[:weight_goal],
-            body_fat_goal: "#{params[:body_fat_goal].to_f/100}",
-            start_calorie_goal: params[:start_calorie_goal],
-            user_id: @user.id
-            )
+        params[:start_bodyfat] = params[:start_bodyfat].to_f/100
+        params[:body_fat_goal] = params[:body_fat_goal].to_f/100
+        params[:user_id] = @user.id
+        @goals = Goal.create(params)
         flash[:notice] = "You have successfully created your goals"
         flash[:navigation] = "Review and edit your goals, view your diary, or continue on to view all user diaries"
         redirect '/goals'
