a40b720afb7d0428ad3abc40ca04c45bdd6b813c
satodd1967 <seth.todd@outlook.com>
2020-08-03 14:56:07 -0500

    added markup to the goals controller

--- app/controllers/goals_controller.rb
+++ app/controllers/goals_controller.rb
@@ -1,11 +1,18 @@
 class GoalsController < ApplicationController
     
+    # This route goes to the show page for goals.  It utilizes the user_check method for validation.
+    
     get '/goals' do
         user_check
         @user = current_user
         @goals = @user.goal
         erb :'goals/goals'
     end
+
+    # This route takes in params from the goals new page.  It first utilizes the user_check method for validation and then
+    # utilizes the check_completion method to make sure that all form fields are completed.
+    # Then the route reformats some fields for database ingestion and then creates goals based on the updated params.
+    # Once created it messages and routes the to the goals show page.
 
     post '/goals' do
         user_check
@@ -20,11 +27,15 @@ class GoalsController < ApplicationController
         redirect '/goals'
     end
 
+    # This route takes the user to the new goals page.  It utilizes the user_check method for validation.
+
     get '/goals/new' do
         user_check
         @user = current_user
         erb :'goals/new'
     end
+
+    # This route takes the user to the goals edit page and utilizes the user_check method for validation
 
     get '/goals/edit' do
         user_check
@@ -32,6 +43,10 @@ class GoalsController < ApplicationController
         @goals = @user.goal
         erb :'goals/edit'
     end
+
+    # This route takes in params from the goals edit page.  It uses the user_check method for validation.
+    # If then uses the check_completion method to ensure all fields have been filled in.
+    # If all checks are passed it updates the users goals, message and routes them to the goals show page.
     
     patch '/goals' do
         user_check
