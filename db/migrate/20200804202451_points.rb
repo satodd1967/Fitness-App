f60595f85478c1bc001399a8fffc86a09dbdf3f6
satodd1967 <seth.todd@outlook.com>
2020-08-04 15:28:37 -0500

    filled in the tbl points migration.

--- db/migrate/20200804202451_points.rb
+++ db/migrate/20200804202451_points.rb
@@ -1,4 +1,12 @@
 class Points < ActiveRecord::Migration
   def change
+    create_table :logs do |t|
+      t.integer :points_worked_out
+      t.integer :points_tracked_food
+      t.integer :points_met_calorie_goal
+      t.integer :points_maintain_weight
+      t.integer :points_maintain_bodyfat
+      t.integer :points_met_active_calories
+    end
   end
 end
