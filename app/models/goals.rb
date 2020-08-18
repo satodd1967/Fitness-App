caf6577bb533cfe99355cd4698b0064ec1c3d2b1
satodd1967 <seth.todd@outlook.com>
2020-08-18 18:03:53 -0500

    added comments in for the convert_create method in the goals model and added comments in for the goals_update method in the goals model.

--- app/models/goals.rb
+++ app/models/goals.rb
@@ -1,12 +1,17 @@
 class Goal < ActiveRecord::Base
     belongs_to :user
 
+    # This method converts the start_bodyfat and the body_fat_goal to a decimal for the database
+    # and then creates goals.
+
     def self.convert_create(params, user)
         params[:start_bodyfat] = params[:start_bodyfat].to_f/100
         params[:body_fat_goal] = params[:body_fat_goal].to_f/100
         params[:user_id] = user.id
         @goals = Goal.create(params)
     end
+
+    # This method does some conversions for the database then updates the goals that were passed into the method
 
     def self.goals_update(params, user, goals)
         goals.update(
